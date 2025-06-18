204155050b1b7154555bf157396764254aede927
Craig Mason <craig.mason@stasismedia.com>
2025-06-18 14:26:33 +0100

    Allow root name to be specified
    Don't create backup file, instead write to suffixed file

--- abcrenamechan.py
+++ abcrenamechan.py
@@ -9,6 +9,7 @@ msg = "Renames an Ornatrix ABC Almbic channel to an Unreal compatible 'groom_col
 # Initialize parser
 parser = argparse.ArgumentParser(description = msg)
 
+parser.add_argument('-g', '--groom_root', help = "Groom name at the root. Defaults to 'GROOMShape'", default = "GROOMShape")
 parser.add_argument('-s', '--source_channel', help = "Source channel to rename. Defaults to 'chan_groom_color_RGB'", default = "chan_groom_color_RGB")
 parser.add_argument('input_file', help = "Input .abc file")
 
@@ -24,15 +25,16 @@ def walk(obj):
         walk(child)
 
 
-bakFile = "{}.bak".format(args.input_file)
-os.rename(src=args.input_file, dst=bakFile)
-
 # Process the main abc file
-a = cask.Archive(bakFile)
+a = cask.Archive(args.input_file)
 
 # Known structure
-geom = a.top.children["GROOMShape"].properties[".geom"]
+geom = a.top.children[args.groom_root].properties[".geom"]
 arbProps = geom.properties['.arbGeomParams']
 arbProps.properties[args.source_channel].name = 'groom_color'
 
-a.write_to_file(args.input_file)
+# Save to a new file
+filename, ext = os.path.splitext(args.input_file)
+new_filename = f"{filename}_color{ext}"
+
+a.write_to_file(new_filename)
